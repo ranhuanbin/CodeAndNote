94e6c8c85b188a6d8ef09c904ad3288d490ca5a8
245123774@qq.com <ruan19900528>
2020-08-10 09:56:40 +0800

    ForOffer02

--- TestModule/src/main/java/com/hb/test/algorithm/AlgorithmTestActivity.kt
+++ TestModule/src/main/java/com/hb/test/algorithm/AlgorithmTestActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import com.hb.test.R
 import com.hb.test.algorithm.foroffer01.ForOffer01
+import com.hb.test.algorithm.foroffer02.ForOffer02
 import kotlinx.android.synthetic.main.activity_test_algorithm.*
 
 class AlgorithmTestActivity : FragmentActivity() {
@@ -11,9 +12,13 @@ class AlgorithmTestActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_algorithm)
         forOffer01.setOnClickListener { forOffer01() }
+        forOffer02.setOnClickListener { forOffer02() }
     }
 
     private fun forOffer01() {
         ForOffer01.test(3);
+    }
+    private fun forOffer02() {
+        ForOffer02.replaceBlank();
     }
 }
--- TestModule/src/main/java/com/hb/test/algorithm/Algotithm.md
+++ TestModule/src/main/java/com/hb/test/algorithm/Algotithm.md
@@ -6,3 +6,8 @@
 https://github.com/LRH1993/android_interview/blob/master/algorithm/For-offer/01.md
 题目描述:
     在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+    
+20200810
+https://github.com/LRH1993/android_interview/blob/master/algorithm/For-offer/02.md
+题目描述:
+    请实现一个函数，把字符串中的每个空格替换成"%20"，例如“We are happy.”，则输出“We%20are%20happy.”。
