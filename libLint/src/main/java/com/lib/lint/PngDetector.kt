bd640e09bdf5f862a5b7ac6458e48bc5ab7c629f
245123774@qq.com <ruan19900528>
2021-07-29 14:10:49 +0800

    kotlin版本匿名内部类PngDetector.createUastHandler

--- libLint/src/main/java/com/lib/lint/PngDetector.kt
+++ libLint/src/main/java/com/lib/lint/PngDetector.kt
@@ -1,6 +1,6 @@
 package com.lib.lint
 
-import com.android.SdkConstants.ATTR_SRC
+import com.android.SdkConstants.*
 import com.android.resources.ResourceFolderType
 import com.android.tools.lint.client.api.UElementHandler
 import com.android.tools.lint.detector.api.*
@@ -13,49 +13,83 @@ import java.io.File
 import java.util.*
 
 class PngDetector : Detector(), XmlScanner, UastScanner {
-    private val LINT_EXPLANATION = "WebP can provide better compression than PNG. "
 
     private val LINT_MSG = "Please use webp instead of png."
 
-    val ISSUE_PNG_IN_XML: Issue = Issue.create(
-        "PngUseInXml",
-        "Png Usage",
-        LINT_EXPLANATION,
-        Category.CORRECTNESS,
-        5,
-        Severity.WARNING,
-        Implementation(PngDetector::class.java, Scope.ALL_RESOURCES_SCOPE)
-    )
+    companion object {
+        const val LINT_EXPLANATION = "WebP can provide better compression than PNG. "
 
-    val ISSUE_PNG_IN_CODE: Issue = Issue.create(
-        "PngUseIncode",
-        "Png Usage",
-        LINT_EXPLANATION,
-        Category.CORRECTNESS,
-        5,
-        Severity.WARNING,
-        Implementation(PngDetector::class.java, Scope.JAVA_FILE_SCOPE)
-    )
+        val ISSUE_PNG_IN_XML: Issue = Issue.create(
+            "PngUseInXml",
+            "Png Usage",
+            LINT_EXPLANATION,
+            Category.CORRECTNESS,
+            5,
+            Severity.WARNING,
+            Implementation(PngDetector::class.java, Scope.ALL_RESOURCES_SCOPE)
+        )
+
+        val ISSUE_PNG_IN_CODE: Issue = Issue.create(
+            "PngUseIncode",
+            "Png Usage",
+            LINT_EXPLANATION,
+            Category.CORRECTNESS,
+            5,
+            Severity.WARNING,
+            Implementation(PngDetector::class.java, Scope.JAVA_FILE_SCOPE)
+        )
+    }
 
     override fun getApplicableAttributes(): Collection<String>? {
-        return Collections.singletonList(ATTR_SRC)
+        println("PngDetector getApplicableAttributes")
+//        return Collections.singletonList(ATTR_SRC)
+        return mutableListOf(ATTR_SRC, ATTR_BACKGROUND)
     }
 
     override fun visitAttribute(context: XmlContext, attribute: Attr) {
+        println("PngDetector visitAttribute")
         val srcValue = attribute.value
-        if (check(srcValue.substring(10), context.mainProject.resourceFolders)) {
+        println("srcValue = $srcValue")
+        if (check(srcValue.substring(10), context.mainProject.resourceFolders)
+            || check(srcValue.substring(8), context.mainProject.resourceFolders)
+        ) {
             context.report(ISSUE_PNG_IN_XML, attribute, context.getLocation(attribute), LINT_MSG)
         }
     }
 
     override fun getApplicableUastTypes(): List<Class<out UElement>>? {
+        val singletonList = Collections.singletonList(UCallExpression::class.java)
+        println("PngDetector getApplicableUastTypes size = ${singletonList.size}")
         return Collections.singletonList(UCallExpression::class.java)
     }
 
-    override fun createUastHandler(context: JavaContext): UElementHandler? {
+    override fun createUastHandler(context: JavaContext): UElementHandler {
+        println("PngDetector createUastHandler")
         return object : UElementHandler() {
             override fun visitCallExpression(node: UCallExpression) {
-
+                if (!KotlinAdapter.isMethodCall(node)
+                    || "setImageResource" != node.methodName
+                    || node.valueArgumentCount != 1
+                ) {
+                    return
+                }
+                val psiMethod = node.resolve()
+                val memberInClass =
+                    context.evaluator.isMemberInClass(psiMethod, "android.widget.ImageView")
+                if (memberInClass) {
+                    if (check(
+                            node.valueArguments.get(0).toString().substring(11),
+                            context.mainProject.resourceFolders
+                        )
+                    ) {
+                        context.report(
+                            ISSUE_PNG_IN_CODE,
+                            node,
+                            context.getLocation(node),
+                            LINT_MSG
+                        )
+                    }
+                }
             }
         }
     }
@@ -91,6 +125,7 @@ class PngDetector : Detector(), XmlScanner, UastScanner {
     }
 
     override fun appliesTo(folderType: ResourceFolderType): Boolean {
+        println("PngDetector appliesTo folderType == $folderType")
         return folderType == ResourceFolderType.LAYOUT
     }
 
