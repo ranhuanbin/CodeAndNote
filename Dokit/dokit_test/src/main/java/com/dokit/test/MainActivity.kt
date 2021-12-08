b47b676b06ad62f10821c169454aa02fef205745
ruanhb@akulaku.com <ruan19900528>
2021-12-08 20:55:11 +0800

    kotlin

--- Dokit/dokit_test/src/main/java/com/dokit/test/MainActivity.kt
+++ Dokit/dokit_test/src/main/java/com/dokit/test/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dokit.test
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.FragmentActivity
 import com.test.kotlin.block.BlockTest
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,7 +12,17 @@ class MainActivity : FragmentActivity() {
         setContentView(R.layout.activity_main)
         blockTest.setOnClickListener {
             BlockTest.test1 {
+                Log.v("AndroidTest", "test1")
+            }
 
+            BlockTest.test2 {
+                Log.v("AndroidTest", "test2")
+                "1"
+            }
+
+            BlockTest.test3 { x, y ->
+                Log.v("AndroidTest", "test3")
+                x + y
             }
         }
     }
--- Dokit/dokit_test/src/main/java/com/test/kotlin/block/BlockTest.kt
+++ Dokit/dokit_test/src/main/java/com/test/kotlin/block/BlockTest.kt
@@ -5,22 +5,31 @@ package com.test.kotlin.block
  */
 object BlockTest {
     /**
-     *
+     * 无入参，无返回值
      */
     fun test1(block: () -> Unit) {
         block()
     }
 
+    /**
+     * 无入参，有返回值
+     */
     fun test2(block: () -> String) {
         val result = block()
         println("test2 ===> result = $result")
     }
 
+    /**
+     * 有入参，有返回值
+     */
     fun test3(block: (x: Int, y: Int) -> Int) {
         val result = block(1, 2)
         println("test3 ===> result = $result")
     }
 
+    /**
+     * 有入参，无返回值
+     */
     fun test4(block: (x: Int, y: Int) -> Unit) {
         block(1, 2)
     }
